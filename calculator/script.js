9bbd8ca1f2cb9e7daaeecc9c34bf461b24f44990
varun kumar <varunkumar19999210@gmail.com>
2024-03-10 21:52:57 +0530

    Initial commit

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('button')
+const screen = document.getElementById('screen')
+
+for( let button of buttons){
+    button.addEventListener('click', (e)=>{
+       const buttonText =e.target.innerText;
+       if(buttonText === 'C'){
+        screen.value = "";
+       }
+       
+       else if(buttonText === 'x'){
+           screen.value += '*';
+           screen.value = eval(screen.value);
+        }
+        else if(buttonText === '='){
+            try{
+         screen.value = eval(screen.value);
+        }
+        catch(error){
+            screen.value = "Invalid Operator";
+        }
+    }
+       else{
+       screen.value += buttonText;
+    }
+    })
+}
